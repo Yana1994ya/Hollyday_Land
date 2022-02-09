c04f01a19be35b099bad4c489863256b15f3a381
Yana <yana11114@gmail.com>
2022-02-09 18:36:05 +0200

    Style filter page.

--- lib/screens/trail/filter.dart
+++ lib/screens/trail/filter.dart
@@ -34,6 +34,8 @@ class _TrailsFilterScreenState extends State<TrailsFilterScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final subtitle = Theme.of(context).textTheme.subtitle1;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Trails filter"),
@@ -41,148 +43,179 @@ class _TrailsFilterScreenState extends State<TrailsFilterScreen> {
           IconButton(
             onPressed: () {
               Navigator.of(context).pop(TrailsFilter(
-                  difficulty: difficulty,
-                  length: lengthRange,
-                  elevationGain: elvGainRange));
+                difficulty: difficulty,
+                length: lengthRange,
+                elevationGain: elvGainRange,
+              ));
             },
             icon: Icon(Icons.save),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pop(TrailsFilter.empty());
+            },
+            icon: Icon(Icons.delete),
           )
         ],
       ),
-      body: Column(
-        children: [
-          Text("Minimum length"),
-          Slider(
-            value: lengthRange.rangeStart == null
-                ? 0.0
-                : lengthRange.rangeStart!.toDouble(),
-            min: 0,
-            max: 20000,
-            divisions: 1000,
-            onChanged: (newValue) {
-              setState(() {
-                if (newValue == 0.0) {
-                  lengthRange = MeterRange(null, lengthRange.rangeEnd);
-                } else {
-                  lengthRange =
-                      MeterRange(newValue.toInt(), lengthRange.rangeEnd);
-                }
-              });
-            },
-            label: lengthRange.rangeStart == null
-                ? "Any"
-                : (lengthRange.rangeStart! / 1000).toString() + "km",
-          ),
-          Text("Maximum length"),
-          Slider(
-            value: lengthRange.rangeEnd == null
-                ? 20000
-                : lengthRange.rangeEnd!.toDouble(),
-            min: 1000,
-            max: 20000,
-            divisions: 1000,
-            onChanged: (newValue) {
-              setState(() {
-                if (newValue == 20000) {
-                  lengthRange = MeterRange(lengthRange.rangeStart, null);
-                } else {
-                  lengthRange =
-                      MeterRange(lengthRange.rangeStart, newValue.toInt());
-                }
-              });
-            },
-            label: lengthRange.rangeEnd == null
-                ? "Any"
-                : (lengthRange.rangeEnd! / 1000).toString() + "km",
-          ),
-          Text("Minimum Elevation Gain"),
-          Slider(
-            value: elvGainRange.rangeStart == null
-                ? 0.0
-                : elvGainRange.rangeStart!.toDouble(),
-            min: 0,
-            max: 4000,
-            divisions: 100,
-            onChanged: (newValue) {
-              setState(() {
-                if (newValue == 0.0) {
-                  elvGainRange = MeterRange(null, elvGainRange.rangeEnd);
-                } else {
-                  elvGainRange =
-                      MeterRange(newValue.toInt(), elvGainRange.rangeEnd);
-                }
-              });
-            },
-            label: elvGainRange.rangeStart == null
-                ? "Any"
-                : (elvGainRange.rangeStart!).toString() + "m",
-          ),
-          Text("Maximum Elevation Gain"),
-          Slider(
-            value: elvGainRange.rangeEnd == null
-                ? 4000
-                : elvGainRange.rangeEnd!.toDouble(),
-            min: 100,
-            max: 4000,
-            divisions: 100,
-            onChanged: (newValue) {
-              setState(() {
-                if (newValue == 4000) {
-                  elvGainRange = MeterRange(elvGainRange.rangeStart, null);
-                } else {
-                  elvGainRange =
-                      MeterRange(elvGainRange.rangeStart, newValue.toInt());
-                }
-              });
-            },
-            label: elvGainRange.rangeEnd == null
-                ? "Any"
-                : (elvGainRange.rangeEnd!).toString() + "m",
-          ),
-          Row(
-            children: [
-              ChoiceChip(
-                label: Text("Easy"),
-                selected: difficulty.contains(Difficulty.easy),
-                onSelected: (value) {
-                  setState(() {
-                    if (value) {
-                      difficulty.add(Difficulty.easy);
-                    } else {
-                      difficulty.remove(Difficulty.easy);
-                    }
-                  });
-                },
-              ),
-              ChoiceChip(
-                label: Text("Moderate"),
-                selected: difficulty.contains(Difficulty.medium),
-                onSelected: (value) {
-                  setState(() {
-                    if (value) {
-                      difficulty.add(Difficulty.medium);
-                    } else {
-                      difficulty.remove(Difficulty.medium);
-                    }
-                  });
-                },
-              ),
-              ChoiceChip(
-                label: Text("Hard"),
-                selected: difficulty.contains(Difficulty.hard),
-                onSelected: (value) {
-                  setState(() {
-                    if (value) {
-                      difficulty.add(Difficulty.hard);
-                    } else {
-                      difficulty.remove(Difficulty.hard);
-                    }
-                  });
-                },
-              ),
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Text(
+              "Trail length - Minimum",
+              style: subtitle,
+            ),
+            Slider(
+              value: lengthRange.rangeStart == null
+                  ? 0.0
+                  : lengthRange.rangeStart!.toDouble(),
+              min: 0,
+              max: 20000,
+              divisions: 1000,
+              onChanged: (newValue) {
+                setState(() {
+                  if (newValue == 0.0) {
+                    lengthRange = MeterRange(null, lengthRange.rangeEnd);
+                  } else {
+                    lengthRange =
+                        MeterRange(newValue.toInt(), lengthRange.rangeEnd);
+                  }
+                });
+              },
+              label: lengthRange.rangeStart == null
+                  ? "Any"
+                  : (lengthRange.rangeStart! / 1000).toString() + "km",
+            ),
+            Divider(),
+            Text(
+              "Trail length - Maximum",
+              style: subtitle,
+            ),
+            Slider(
+              value: lengthRange.rangeEnd == null
+                  ? 20000
+                  : lengthRange.rangeEnd!.toDouble(),
+              min: 1000,
+              max: 20000,
+              divisions: 1000,
+              onChanged: (newValue) {
+                setState(() {
+                  if (newValue == 20000) {
+                    lengthRange = MeterRange(lengthRange.rangeStart, null);
+                  } else {
+                    lengthRange =
+                        MeterRange(lengthRange.rangeStart, newValue.toInt());
+                  }
+                });
+              },
+              label: lengthRange.rangeEnd == null
+                  ? "Any"
+                  : (lengthRange.rangeEnd! / 1000).toString() + "km",
+            ),
+            Divider(),
+            Text(
+              "Elevation gain - Minimum",
+              style: subtitle,
+            ),
+            Slider(
+              value: elvGainRange.rangeStart == null
+                  ? 0.0
+                  : elvGainRange.rangeStart!.toDouble(),
+              min: 0,
+              max: 4000,
+              divisions: 100,
+              onChanged: (newValue) {
+                setState(() {
+                  if (newValue == 0.0) {
+                    elvGainRange = MeterRange(null, elvGainRange.rangeEnd);
+                  } else {
+                    elvGainRange =
+                        MeterRange(newValue.toInt(), elvGainRange.rangeEnd);
+                  }
+                });
+              },
+              label: elvGainRange.rangeStart == null
+                  ? "Any"
+                  : (elvGainRange.rangeStart!).toString() + "m",
+            ),
+            Divider(),
+            Text(
+              "Elevation gain - Maximum",
+              style: subtitle,
+            ),
+            Slider(
+              value: elvGainRange.rangeEnd == null
+                  ? 4000
+                  : elvGainRange.rangeEnd!.toDouble(),
+              min: 100,
+              max: 4000,
+              divisions: 100,
+              onChanged: (newValue) {
+                setState(() {
+                  if (newValue == 4000) {
+                    elvGainRange = MeterRange(elvGainRange.rangeStart, null);
+                  } else {
+                    elvGainRange =
+                        MeterRange(elvGainRange.rangeStart, newValue.toInt());
+                  }
+                });
+              },
+              label: elvGainRange.rangeEnd == null
+                  ? "Any"
+                  : (elvGainRange.rangeEnd!).toString() + "m",
+            ),
+            Divider(),
+            Text(
+              "Difficulty",
+              style: subtitle,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ChoiceChip(
+                  label: Text("Easy"),
+                  selected: difficulty.contains(Difficulty.easy),
+                  onSelected: (value) {
+                    setState(() {
+                      if (value) {
+                        difficulty.add(Difficulty.easy);
+                      } else {
+                        difficulty.remove(Difficulty.easy);
+                      }
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text("Moderate"),
+                  selected: difficulty.contains(Difficulty.medium),
+                  onSelected: (value) {
+                    setState(() {
+                      if (value) {
+                        difficulty.add(Difficulty.medium);
+                      } else {
+                        difficulty.remove(Difficulty.medium);
+                      }
+                    });
+                  },
+                ),
+                ChoiceChip(
+                  label: Text("Hard"),
+                  selected: difficulty.contains(Difficulty.hard),
+                  onSelected: (value) {
+                    setState(() {
+                      if (value) {
+                        difficulty.add(Difficulty.hard);
+                      } else {
+                        difficulty.remove(Difficulty.hard);
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
