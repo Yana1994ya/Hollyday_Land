5c7f783c9d6dedf0f3b889ca25ef2a15ce6cd50c
Yana <yana11114@gmail.com>
2022-04-13 20:39:23 +0300

    Add models for tours.

---
+++ lib/models/tour/overnight.objects.filter_tags.dart
@@ -0,0 +1,19 @@
+part of "overnight.dart";
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// FilterTagDaoGenerator
+// **************************************************************************
+
+class _OvernightDao extends FilterTagModelAccess<Overnight> {
+  const _OvernightDao();
+
+  @override
+  Overnight fromJson(Map<String, dynamic> json) => Overnight.fromJson(json);
+
+  @override
+  String get modelName => "overnights";
+}
+
+const FilterTagModelAccess<Overnight> overnightObjects = _OvernightDao();
---
+++ lib/models/tour/package.objects.filter_tags.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// FilterTagDaoGenerator
+// **************************************************************************
+
+part of "package.dart";
+
+class _PackageDao extends FilterTagModelAccess<Package> {
+  const _PackageDao();
+
+  @override
+  Package fromJson(Map<String, dynamic> json) => Package.fromJson(json);
+
+  @override
+  String get modelName => "packages";
+}
+
+const FilterTagModelAccess<Package> packageObjects = _PackageDao();
