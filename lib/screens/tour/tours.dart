871f3fde467b762f0b18cebca26d4658882430ce
Yana <yana11114@gmail.com>
2022-05-03 22:08:26 +0300

    Fix extreme sports.
    Add under-development message for tours.

--- lib/screens/tour/tours.dart
+++ lib/screens/tour/tours.dart
@@ -1,11 +1,19 @@
 import "package:flutter/material.dart";
+import "package:hollyday_land/widgets/no_results.dart";
 
 class ToursScreen extends StatelessWidget {
   static const routePath = "/tours";
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tours"),
+      ),
+      body: NoResults(
+        text: "Coming soon",
+        subTitle: "This feature is currently under development",
+      ),
+    );
   }
 }
